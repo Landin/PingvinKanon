9a05d1e13c32054b09d4f83c9705a071081df7e3
DESKTOP-AR3V2MK\Timestop <axel@timnestopinteractive.com>
2017-10-01 16:29:44 +0200

    Penguin throwing and flying logic needs setup in scene

---
+++ PingvinKanonProj/Assets/PenguinMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenguinMove : MonoBehaviour
+{
+
+    bool m_Launched = false;
+    bool m_Landed = false;
+
+    public float m_FlySpeed;
+
+    float m_DistanceToFly = 0.0f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (m_Launched)
+        {
+            transform.position += new Vector3(Time.deltaTime * m_FlySpeed, 0, 0);
+            m_DistanceToFly -= Time.deltaTime * m_FlySpeed;
+        }
+
+        if(m_DistanceToFly <= 0)
+        {
+            m_Launched = false;
+            m_Landed = true;
+        }
+    }
+
+    public void Launch(float i_Distance)
+    {
+        m_Launched = true;
+        m_DistanceToFly = i_Distance;
+    }
+}
---
+++ PingvinKanonProj/Assets/PenguinToss.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenguinToss : MonoBehaviour
+{
+
+    public GameObject m_Penguin;
+    public float m_MaxFlyDistance;
+    public float m_FlyDistance;
+
+    bool m_TimerActive = false;
+
+    public float m_TimeToFullCharge;
+    float m_Time = 0.0f;
+
+	void Start ()
+    {
+
+	}
+	
+	void Update ()
+    {
+        if(m_TimerActive)
+        {
+            m_Time += Time.deltaTime;
+        }
+		if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            m_TimerActive = true;
+        }
+        else if(Input.GetKeyUp(KeyCode.Mouse0))
+        {
+
+            if (m_Time > m_TimeToFullCharge)
+                m_Time = m_TimeToFullCharge;
+
+            m_FlyDistance = (m_Time / m_TimeToFullCharge) * m_MaxFlyDistance;
+
+            GameObject penguin = Instantiate(m_Penguin);
+            penguin.transform.position = transform.position;
+            penguin.SendMessage("Launch", m_FlyDistance);
+
+            m_TimerActive = false;
+            m_Time = 0.0f;
+        }
+	}
+}
